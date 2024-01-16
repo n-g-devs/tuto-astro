156316260fb669ea8826c4b7227b5f6ba4ba9aec
Lisa Scelli <lisa.scelli@lean-coders.at>
2024-01-16 13:57:30 +0100

    add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://chipper-axolotl-7e3d58.netlify.app/"
+  site: "https://chipper-axolotl-7e3d58.netlify.app/",
+  integrations: [preact()]
 });

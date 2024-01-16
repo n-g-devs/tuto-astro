a01f136802c9b50cbdc83db45b7b1178435eb0fa
Lisa Scelli <lisa.scelli@lean-coders.at>
2024-01-16 13:53:01 +0100

    add rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://chipper-axolotl-7e3d58.netlify.app/"
+});
